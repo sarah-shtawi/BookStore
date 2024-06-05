50941bc9577e51a1ea2c3768294460ee72008587
sarah-shtawi <sarah.sht03@gmail.com>
2024-06-05 03:51:37 +0300

    create and Edit

--- BookStore/Controllers/CategoriesController.cs
+++ BookStore/Controllers/CategoriesController.cs
@@ -1,4 +1,6 @@
 ﻿using BookStore.Data;
+using BookStore.Models;
+using BookStore.ViewModel;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BookStore.Controllers
@@ -15,6 +17,46 @@ namespace BookStore.Controllers
         {
             var categories = context.categories.ToList();
             return View(categories);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View(); 
+        }
+
+        [HttpPost]
+        public IActionResult Create(CategoryVM categoryVM)
+        {
+            if(!ModelState.IsValid) 
+            {
+                return View("Create", categoryVM); 
+            }
+            var Category = new Category
+            {
+               Name = categoryVM.Name
+            };
+            context.categories.Add(Category);//DB
+            context.SaveChanges();
+            return RedirectToAction("Index"); 
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int Id)
+        {
+            return View("Create");
+        }
+        [HttpPost]
+        public IActionResult Edit(CategoryVM categoryvm)
+        {
+            var Category = context.categories.Find(categoryvm.Id);
+            if(Category is null)
+            {
+                return NotFound();
+            }
+            Category.Name = categoryvm.Name;
+            context.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ BookStore/ViewModel/CategoryVM.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.ViewModel
+{
+    public class CategoryVM
+    {
+
+        public int Id { get; set; }
+        [MaxLength(30,ErrorMessage="Max Langth 30 characters")]
+        [Required(ErrorMessage= "Name is Required")]
+        public string Name { get; set; } = null!;
+    }
+}
